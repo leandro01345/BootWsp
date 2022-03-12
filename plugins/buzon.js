8dd485157d1742c9b03281ccdda1216e5df5f33c
leandro01345 <hideki_hinata-@hotmail.com>
2022-03-12 00:00:13 -0300

    Initial commit

---
+++ plugins/buzon.js
@@ -0,0 +1,245 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `
+*Mayor Lista de Buzones*
+Actualizados
+╔════⪼ཊ༼⚚༿𖤍༾⚚༽ཏ⪻════╗
+Lista by: ⍣𝑻𝒐𝒏𝒚𝒙|ɴʙᴊ ×͜×⍣
+•Incluído By @Jhusz
+--------------------------------------------------------
+EE.UU +1
+Se interrumpe 3 veces con *
+De coloca el número sin el +1 
+1 para Español 
+Prede: Últimos 4 dígitos#
+--------------------------------------------------------
+Mexico +52
+Compañía: Telcel 
+Buzón externo: *+52 222 522 2222*
+Se coloca el número sin el +52 y se cierra con #
+Prede: 1234#
+--------------------------------------------------------
+Guatemala +502
+Compañía: Claro
+Buzón directo 
+Se interrumpe con #
+Prede: 888888#
+
+*Bases comunes*
++502 42xx xxxx
++502 41xx xxxx
+
+*Bases anti reset (OFC)*
++502 36xx xxxx 
++502 37xx xxxx
++502 56xx xxxx
++502 58xx xxxx
++502 59xx xxxx
+
+¿ Dificultad del buzón ?: Fácil 
+# 888888 # Nueva clave #
+
+*9* - 2 Para cambiar la clave. 
+*1* - Para escuchar mensajes
+*3* - Para reproducir el siguiente mensaje
+*7*  - Para borrar todos los mensajes
+--------------------------------------------------------
+El Salvador +503
+Compañía: Movistar y Tigo
+Buzón directo 
+Se interrumpe con *
+Prede: todo el num sin el +503#
+--------------------------------------------------------
+Honduras +504 (CLARO)
+Compañía: Claro
+Se interrumpe con #
+Prede: 888888#
+
+Bases Claro 
++504 32XX XXXX
++504 8XXX XXXX
++504 338X XXXX
+
+los mensajes se reproducen solos 
+2 - para reproducir de nuevo el mensaje 
+3 - para reproducir el siguiente mensaje 
+7 - 7 para borrar todos los mensajes 
+9 - 2 para cambiar clave 
+la prede es # 888888
+
+Honduras +504 (TIGO)
+Compañía: Tigo
+Buzón directo
+Se interrumpe con *
+Prede: 1234# o 2010#
+--------------------------------------------------------
+Costa Rica +506
+Compañía: Claro
+Buzón directo
+Se interrumpe con #
+Prede: 888888#
+--------------------------------------------------------
+Panamá +507
+Compañía: +Movil
+Buzón externo: *+507 214-6666* 
+Ponemos el num sin el +507#
+1 Para Español 
+Prede: No tiene, se crea al entrar.
+--------------------------------------------------------
+San Pedro y Miquelon +508
+Buzón externo: *+508 41 31 25* 
+Se interrumpe con #
+Ponemos el num con 508# 
+Prede: 0000#
+--------------------------------------------------------
+Haití +509
+Compañía: Digicel
+Buzón externo: *+509 37 00 0003* 
+Ponemos el num com el 509# 
+Presionamos 1 
+Prede: 0000 
+--------------------------------------------------------
+Colombia +57
+Buzon directo 
+Al entrar nos dicta los mensajes con 1 
+9 - 4 para nueva prede
+#2 para confirmar 
+Activar contra 9 - 8 
+--------------------------------------------------------
+Argentina +54 (AMX)
+Compañía: amx
+Buzón directo
+Se interrumpe con *
+Prede: 1111
+
+Argentina +54 (TELECOM)
+Compañía: Telecom
+Buzón directo
+Se interrumpe con *
+Prede: Últimos 4 dígitos#
+--------------------------------------------------------
+Uruguay +598 Fijos
+Buzon directo. 
+Se interrumpe con * 
+Prede: Últimos 4 dígitos 
+--------------------------------------------------------
+Brasil +55
+Buzon directo. 
+Se interrumpe con * 
+Prede: 9999#
+1 para escuchar los sms 
+--------------------------------------------------------
+Chile +56
+Compañía: Claro 
+Buzon directo. 
+Se interrumpe con: ** 
+Prede: 1234#
+--------------------------------------------------------
+Reino Unido +44
+Buzon directo. 
+Se interrumpe con * 
+Prede: 1234#
+1 para escuchar sms 
+--------------------------------------------------------
+España +34
+Compañía: Tuenti 
+Buzon externo: *+34 684 32 21 23*
+Ponemos el num sin el +34 cerramos con #
+Prede: Últimos 4 dígitos 
+--------------------------------------------------------
+Bélgica +32
+Buzon externo: *+32 475 15 19 69*
+Ponemos el num sin +32 
+Prede: 0000#
+--------------------------------------------------------
+Francia +33
+Base 33 6
+Se interrumpe con * 
+Prede: 0000#
+
+Base 33 9
+Buzón directo. 
+Se interrumpe con # 
+Prede: 0000#
+--------------------------------------------------------
+Austria +43
+Buzon externo: *+43 650 11 000* 
+Ponemos el num sin el +43 
+Cerramos con # 
+Prede: 1234# 0000# o últimos 4 dígitos# 
+--------------------------------------------------------
+Groenlandia +299
+Buzon externo: *+299 50 50 50* 
+Ponemos el num sin el +299, cerramos con #
+Prede: 1234#
+--------------------------------------------------------
+Taiwan +886
+Buzón externo: *+886 937 000 777* 
+Ponemos el num sin el +886 pero con un 0 por delante#2 
+Prede: 0000#
+--------------------------------------------------------
+Suiza +41
+Buzón directo. 
+Se interrumpe con * 
+Prede: 1234# o 0000# 
+1 para escuchar sms 
+--------------------------------------------------------
+Malta +356
+Buzón externo: *+356 9990 9910*
+Prede: 9999#
+--------------------------------------------------------
+Gibraltar +350
+Buzón externo: *+350 200 41234*
+Prede: 12345*
+--------------------------------------------------------
+Grecia +30
+Buzón externo: *+30 693 560 1222* 
+Prede: 1111*
+--------------------------------------------------------
+Dinamarca +45
+Buzón externo: *+45 51 10 00 77*
+Prede: 1234# o Últimos 4 dígitos#
+--------------------------------------------------------
+Islas Malvinas +500
+Buzón directo. 
+Se interrumpe con * 
+Prede: 1234#
+--------------------------------------------------------
+Chipre +357
+Compañía: Epic mtn
+Buzón externo: *+357 96 121121*
+Prede: #1111, #1234 o #Últimos 4 dígitos 
+--------------------------------------------------------
+Italia +39
+Buzón externo: *+39 348 200 2020*
+Prede: 123456#
+--------------------------------------------------------
+Malasia +60
+Buzón directo 
+Compañía: Celcom
+Se interrumpe con *
+Prede: 1234#, 1313# o Últimos 4 dígitos#
+--------------------------------------------------------
+Hungria +36
+Compañía: Magyar Telekom
+Buzon Externo: *+36 1 490 1718*
+Interrumpimos con * 
+Prede: 1234# o últimos 4 digitos#
+--------------------------------------------------------
+Marruecos +212
+Compañías: Maroc Telecom 
+Buzón Externo: +212 670 333 333 
+Se interrumpe con *
+Prede: 1234#, últimos 4 digitos#
+--------------------------------------------------------
+Vietnam +84
+Buzón Externo: *+84 0923452828*
+Prede: Últimos 4 dígitos#
+--------------------------------------------------------
+╚════⪼ཊ༼⚚༿𖤍༾⚚༽ཏ⪻════╝
+`.trim() 
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 JHUSZ X-X*', 'status@broadcast')
+}
+handler.command = /^(buzon|buzoneo|buzones)$/i
+
+module.exports = handler
